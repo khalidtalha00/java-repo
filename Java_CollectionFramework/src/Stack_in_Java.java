2ef6d6ea15c25daf310b1f380297faf9a2d25c52
Talha Khalid <117529116+khalidtalha00@users.noreply.github.com>
2024-08-12 18:51:16 +0530

    second commmit

---
+++ Java_CollectionFramework/src/Stack_in_Java.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class Stack_in_Java {
+    public static void main(String[] args) {
+        Stack<String> myStack = new Stack<>();
+
+        myStack.push("Lion");   // add elephants to the top of the stack
+        myStack.push("Tiger");
+        myStack.push("Cat");
+        myStack.push("Elephant");
+        System.out.println("Stack: "+myStack);
+
+        System.out.println(myStack.peek()); // show the top of the stack
+
+        myStack.pop(); // deletes the element from the top of the stack
+        System.out.println(myStack);  // elephant got popped
+
+
+    }
+}
